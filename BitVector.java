bd540f4e46a22188d2e42ddd455fdb28cd49006c
Hayk Gevorgyan <139261784+Hayk-Gevorgyan@users.noreply.github.com>
2024-03-17 01:13:21 +0400

    Update BitVector.java

--- BitVector.java
+++ BitVector.java
@@ -4,16 +4,21 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class BitVector {
-    private static int[] vector = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-    private static int size;
-    private BitVector(int initialSize) {
-        vector = new int[initialSize];
-        for (int i = 0; i < initialSize; i++) {
-            vector[i] = 0;
-        }
-        size = initialSize;
+    public static void main(String[] args) {
+        new BitVector().start();
     }
-    private static void reset(int index) {
+    private static final int DEFAULT_INITIAL_SIZE = 8;
+    private int[] vector = null;
+    private int size = 0;
+    private static final String COMMANDS = """
+    -s _ sets the bit at given index.
+    -r _ resets the bit at given index.
+    -e _ ends the work with bit vector and saves as "yourBitVector.txt"
+    """;
+    private BitVector() {
+
+    }
+    private void reset(int index) {
         try {
             vector[index] = 0;
         }
@@ -21,7 +26,7 @@ public class BitVector {
             System.out.println("index not met");
         }
     }
-    private static void set(int index) {
+    private void set(int index) {
         try {
             vector[index] = 1;
         }
@@ -29,30 +34,51 @@ public class BitVector {
             System.out.println("index not met");
         }
     }
-    public static void start() {
+    public void start() {
         Scanner scan = new Scanner(System.in);
-        Scanner scan1 = new Scanner(System.in);
+        Scanner index = new Scanner(System.in);
         boolean isFinished = false;
         System.out.println("please enter the initial size of your bit vector.");
-        new BitVector(scan1.nextInt());
-        System.out.println("-s _ sets the bit at given index.\n-r _ resets the bit at given index.\n-e _ ends the work with bit vector and saves as \"yourBitVector.txt\"");
+        int size = scan.nextInt();
+        if (size > 0) {
+            this.size = size;
+        } else {
+            this.size = DEFAULT_INITIAL_SIZE;
+        }
+        vector = new int[size];
+        System.out.println("array size is set to " + size);
+        scan.nextLine();
+
         while (!isFinished) {
+            System.out.println(COMMANDS);
             switch (scan.nextLine()) {
                 case "-s" -> {
                     System.out.println("please enter the index to set");
-                    set(scan1.nextInt());
+                    int ind = index.nextInt();
+                    if (ind < size && ind > -1) {
+                        set(ind);
+                        System.out.println("bit at index " + ind + " is set");
+                    } else {
+                        System.out.println("index is less than 0 or more than " + this.size);
+                    }
                 }
                 case "-r" -> {
                     System.out.println("please enter the index to reset");
-                    reset(scan1.nextInt());
+                    int ind = index.nextInt();
+                    if (ind < size && ind > -1) {
+                        reset(ind);
+                        System.out.println("bit at index " + ind + " is reset");
+                    } else {
+                        System.out.println("index is less than 0 or more than " + this.size);
+                    }
                 }
                 case "-e" -> {
                     try {
                         BufferedWriter writer = new BufferedWriter(new FileWriter("yourBitVector.txt"));
-                        writer.write(toStr());
+                        writer.write(toString());
                         writer.close();
                     } catch (IOException e) {
-                        e.printStackTrace();
+                        System.out.println(e.getMessage());
                     }
                     isFinished = true;
                 }
@@ -60,7 +86,8 @@ public class BitVector {
             }
         }
     }
-    private static String toStr() {
+    @Override
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(vector[size - 1]);
         for (int i = size - 2; i >= 0; i--) {
